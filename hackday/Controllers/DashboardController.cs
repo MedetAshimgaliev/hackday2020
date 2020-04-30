c2efe14ded29865c6e86b8441c6a9da564077cbe
MedetAshimgaliev <kachelikachayt@gmail.com>
2020-04-30 17:40:53 +0600

    +11

--- hackday/Controllers/DashboardController.cs
+++ hackday/Controllers/DashboardController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using hackday.Models;
 using hackday.Data;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -20,17 +21,57 @@ namespace hackday.Controllers
             return View();
         }
 
+        public IActionResult AddCourse()
+        {
+            return View();
+        }
+
         [HttpGet]
         public JsonResult GetAllCourses()
         {
-            List<Course> list = new List<Course>();
+            //List<Course> list = new List<Course>();
+            
             using (var entities = new ApplicationDbContext())
             {
-                list = entities.Course.Where(c => c.DeletedDate == null).ToList();
+                //list = entities.Course.Where(c => c.DeletedDate == null).ToList();
+                var list = entities.Course.ToList();
+                return Json(list);
             }
-
-            return Json(list);
-            
         }
+
+        [HttpPost]
+        public async Task<JsonResult> SaveCourse(Course item)
+        {
+            try
+            {
+                Course edit = new Course();
+                using (var entities = new ApplicationDbContext())
+                {
+                    if (item.CourseId > 0)
+                    {
+                        edit = await entities.Course.FirstOrDefaultAsync(c => c.DeletedDate == null && c.CourseId == item.CourseId);
+                    }
+                    else
+                    {
+                        edit.CreatedDate = DateTime.Now;
+                    }
+
+                    edit.Title = item.Title;
+                    edit.Description = item.Description;
+
+                    entities.Course.Add(edit);
+                    await entities.SaveChangesAsync();
+                }
+
+                return Json("done");
+            }
+            catch
+            {
+                return Json("error");
+            }
+        }
+
+
+        
     }
 }
--- hackday/Data/ApplicationDbContext.cs
+++ hackday/Data/ApplicationDbContext.cs
@@ -3,14 +3,36 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using hackday.Models;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace hackday.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public ApplicationDbContext()
+        {
+        }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite("Data Source=app.db"));
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlite("Data Source=app.db");
+            }
+        }
+
+        public DbSet<Course> Course { get; set; }
+        public DbSet<Lesson> Lesson { get; set; }
     }
 }
